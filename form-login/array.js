816be805eee00ba0b4736c3a6a11caff39a15f21
gerinsp <denapratama7@gmail.com>
2019-06-15 16:01:44 +0700

    commit pertama

---
+++ form-login/array.js
@@ -0,0 +1,11 @@
+function panggilArray(){
+    var data = [1,2,3,4,5,6,7,9];
+    var jalan = ['jalan1','jalan2','jalan3'];
+
+    console.log(data[2])
+    console.log(jalan)
+
+    console.log(data.length);
+}
+
+panggilArray();
